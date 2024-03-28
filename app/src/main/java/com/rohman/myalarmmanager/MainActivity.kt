849e5a9f72ca507103ead724fe4aad5a1644db9f
nurmangg <rohmanuyeoke@gmail.com>
2024-03-28 11:34:56 +0700

    Update: Feature Cancelling AlarmManager

--- app/src/main/java/com/rohman/myalarmmanager/MainActivity.kt
+++ app/src/main/java/com/rohman/myalarmmanager/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rohman.myalarmmanager
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,16 +11,37 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
-class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragment.DialogDateListener, TimePickerFragment.DialogTimeListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener,
+    DatePickerFragment.DialogDateListener, TimePickerFragment.DialogTimeListener {
 
     private var binding: ActivityMainBinding? = null
     private lateinit var alarmReceiver: AlarmReceiver
 
+    companion object {
+        private const val DATE_PICKER_TAG = "DatePicker"
+        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
+        private const val TIME_PICKER_REPEAT_TAG = "TimePickerRepeat"
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
 
         // Listener one time alarm
         binding?.btnOnceDate?.setOnClickListener(this)
@@ -29,6 +51,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
         // Listener repeating alarm
         binding?.btnRepeatingTime?.setOnClickListener(this)
         binding?.btnSetRepeatingAlarm?.setOnClickListener(this)
+        binding?.btnCancelRepeatingAlarm?.setOnClickListener(this)
 
         alarmReceiver = AlarmReceiver()
     }
@@ -39,29 +62,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
                 val datePickerFragment = DatePickerFragment()
                 datePickerFragment.show(supportFragmentManager, DATE_PICKER_TAG)
             }
+
             R.id.btn_once_time -> {
                 val timePickerFragmentOne = TimePickerFragment()
                 timePickerFragmentOne.show(supportFragmentManager, TIME_PICKER_ONCE_TAG)
             }
+
             R.id.btn_set_once_alarm -> {
                 val onceDate = binding?.tvOnceDate?.text.toString()
                 val onceTime = binding?.tvOnceTime?.text.toString()
                 val onceMessage = binding?.edtOnceMessage?.text.toString()
 
-                alarmReceiver.setOneTimeAlarm(this, AlarmReceiver.TYPE_ONE_TIME,
+                alarmReceiver.setOneTimeAlarm(
+                    this, AlarmReceiver.TYPE_ONE_TIME,
                     onceDate,
                     onceTime,
-                    onceMessage)
+                    onceMessage
+                )
             }
+
             R.id.btn_repeating_time -> {
                 val timePickerFragmentRepeat = TimePickerFragment()
                 timePickerFragmentRepeat.show(supportFragmentManager, TIME_PICKER_REPEAT_TAG)
             }
+
             R.id.btn_set_repeating_alarm -> {
                 val repeatTime = binding?.tvRepeatingTime?.text.toString()
                 val repeatMessage = binding?.edtRepeatingMessage?.text.toString()
-                alarmReceiver.setRepeatingAlarm(this, AlarmReceiver.TYPE_REPEATING,
-                    repeatTime, repeatMessage)
+                alarmReceiver.setRepeatingAlarm(
+                    this, AlarmReceiver.TYPE_REPEATING,
+                    repeatTime, repeatMessage
+                )
+            }
+
+            R.id.btn_cancel_repeating_alarm -> {
+                alarmReceiver.cancelAlarm(this, AlarmReceiver.TYPE_REPEATING)
             }
         }
     }
@@ -99,11 +134,5 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
         super.onDestroy()
 
         binding = null
-    }
-
-    companion object {
-        private const val DATE_PICKER_TAG = "DatePicker"
-        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
-        private const val TIME_PICKER_REPEAT_TAG = "TimePickerRepeat"
     }
 }
